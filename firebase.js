ffab520cbe4a237021f80bfe13c883506861e0a7
Biolive11 <animeshg402@gmail.com>
2026-01-06 00:25:00 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -24,10 +24,27 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 <script>
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    appId: "YOUR_APP_ID"
+    // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdEJCBY_dNz5pNTcfTMOmCMfcvZIhC_iQ",
+  authDomain: "biolive-mlm.firebaseapp.com",
+  projectId: "biolive-mlm",
+  storageBucket: "biolive-mlm.firebasestorage.app",
+  messagingSenderId: "1092435698858",
+  appId: "1:1092435698858:web:6d35a8a25920bc3b6d5cd3",
+  measurementId: "G-4GG1Z6Y52R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
   };
 
   firebase.initializeApp(firebaseConfig);
